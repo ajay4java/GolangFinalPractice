7babd7e279aa6a6317247eece218e360a0ebf9fd
Ajay Tiwary <ajay4java@gmail.com>
2024-05-23 20:56:43 +0530

    lesson2

---
+++ lesson2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myString string
+
+	fmt.Println(myString)
+
+	myString = "Ajay Tiwary"
+
+	fmt.Println(myString)
+
+	var myInt int = 10
+
+	fmt.Println(myInt)
+	var myBool bool
+
+	fmt.Println(myBool)
+
+}
